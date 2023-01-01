22e2f1a28492410d15ad826d8ba14d8094342792
Samed Dağlı <samedddagl@gmail.com>
2023-01-01 14:09:52 +0300

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Samed Dağlı on 1.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
